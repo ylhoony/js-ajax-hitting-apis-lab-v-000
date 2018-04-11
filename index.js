238519bf6a5d0c4e4a5d45ef4bcf937ed7df8a1a
Hoon Lee <yl.hoony@gmail.com>
2018-04-11 23:38:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function getRepositories() {
   // console.log(username);
 
   const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
   req.open("GET", `https://api.github.com/users/${username}/repos`);
   req.send();
-  req.addEventListener('load', displayRepositories);
 }
 
 function displayRepositories() {
